6e8a4df75c163fd2a7a71480a3cd0ab53d27e884
D32SSV <131158288+D32SSV@users.noreply.github.com>
2024-05-02 23:02:24 +0530

    till 29:45

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,33 @@
-import express from "express";
+import express, { response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  console.log(req);
-  res.status(202).send("Welcome to my world");
+//Middleware for parsing request body to json
+app.use(express.json());
+
+//middileware for handling routes
+app.use("/books", booksRoute);
+
+//middleware for handling CORS Policy
+//Option 1. Allows all origins with default of CORS
+app.use(cors());
+//Option 2. Allow custom origins
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "PUT", "POST", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
+
+app.get("/", (request, response) => {
+  console.log(request);
+  response.status(202).send("Welcome to my world");
 });
 
 mongoose
